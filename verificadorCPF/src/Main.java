98646916a300a22eb80da8c3f8395242c5450d2a
nbeverton <104376567+nbeverton@users.noreply.github.com>
2023-01-09 15:16:51 -0300

    Entrada de dados pelo usuário
    Modificação do código para possibilitar a entrada de dados direto pelo usuário através do programa.

--- verificadorCPF/src/Main.java
+++ verificadorCPF/src/Main.java
@@ -1,48 +1,69 @@
-public class Main {
+package br.com.nbeverton;
+import java.util.Scanner;
+
+public class VerificadorDeCPF {
     public static void main(String[] args) {
-//Preciso saber se o cpf é real
-//Primeiro passo: Multiplar digitos de acordo com a tabela
 
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o seu CPF: ");
+        String CPF = entrada.nextLine();
 
-    int primeiroDigito = 5;
-    int segundoDigito = 7;
-    int terceiroDigito = 2;
-    int quartoDigito = 3;
-    int quintoDigito = 6;
-    int sextoDigito = 7;
-    int setimoDigito = 4;
-    int oitavoDigito = 1;
-    int nonoDigito = 4;
-    int decimoDigito = 4;
-    int decimoPrimeiroDigito = 2;
+        String valor1 = String.valueOf(CPF.charAt(0));
+        Integer digito1 = Integer.parseInt(valor1);
+        String valor2 = String.valueOf(CPF.charAt(1));
+        Integer digito2 = Integer.parseInt(valor2);
+        String valor3 = String.valueOf(CPF.charAt(2));
+        Integer digito3 = Integer.parseInt(valor3);
+        String valor4 = String.valueOf(CPF.charAt(3));
+        Integer digito4 = Integer.parseInt(valor4);
+        String valor5 = String.valueOf(CPF.charAt(4));
+        Integer digito5 = Integer.parseInt(valor5);
+        String valor6 = String.valueOf(CPF.charAt(5));
+        Integer digito6 = Integer.parseInt(valor6);
+        String valor7 = String.valueOf(CPF.charAt(6));
+        Integer digito7 = Integer.parseInt(valor7);
+        String valor8 = String.valueOf(CPF.charAt(7));
+        Integer digito8 = Integer.parseInt(valor8);
+        String valor9 = String.valueOf(CPF.charAt(8));
+        Integer digito9 = Integer.parseInt(valor9);
+        String valor10 = String.valueOf(CPF.charAt(9));
+        Integer digito10 = Integer.parseInt(valor10);
+        String valor11 = String.valueOf(CPF.charAt(10));
+        Integer digito11 = Integer.parseInt(valor11);
 
-    int verificador1;
-    int verificador2;
+        int verificador1;
+        int verificador2;
 
-    int S1 = (primeiroDigito*10 + segundoDigito*9 + terceiroDigito*8 + quartoDigito*7 + quintoDigito*6 + sextoDigito*5 +
-            setimoDigito*4 + oitavoDigito*3 + nonoDigito*2) % 11;
+        int S1 = (digito1*10 + digito2*9 + digito3*8 + digito4*7 +
+                digito5*6 + digito6*5 + digito7*4 + digito8*3 +
+                digito9*2) % 11;
 
-    if (S1 == 0 || S1 == 1){
+        if (S1 == 0 || S1 == 1){
             verificador1 = 0;
         }else {
             verificador1 = 11-S1;
-    }
-    System.out.println(verificador1);
+        }
+        System.out.println(verificador1);
 
-    int S2 = (primeiroDigito*11 + segundoDigito*10 + terceiroDigito*9 + quartoDigito*8 + quintoDigito*7 + sextoDigito*6 +
-            setimoDigito*5 + oitavoDigito*4 + nonoDigito*3 + decimoDigito*2) % 11;
+        int S2 = (digito1*11 + digito2*10 + digito3*9 + digito4*8 +
+                digito5*7 + digito6*6 + digito7*5 + digito8*4 +
+                digito9*3 + digito10*2) % 11;
 
-    if(S2 == 0 || S2 == 1){
-        verificador2 = 0;
-    }else {
-        verificador2 = 11-S2;
-    }
-    System.out.println(verificador2);
+        if (S2 == 0 || S2 == 1){
+            verificador2 = 0;
+        } else {
+            verificador2 = 11-S2;
+        }
 
-    if (verificador1 == decimoDigito && verificador2 == decimoPrimeiroDigito){
-        System.out.println("O número informado é de um CPF válido");
-    }else {
-        System.out.println("O número informado não pertence a um CPF válido. Por favor, reveja o número digitado");
-    }
+        System.out.println(verificador2);
+
+
+        if (verificador1 == digito10 && verificador2 == digito11){
+            System.out.println("O número de CPF digitado é válido");
+        } else {
+            System.out.println("Por favor, digite um número de CPF válido.");
+        }
+
+        entrada.close();
     }
 }
